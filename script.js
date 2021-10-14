eca19d84ecdfeba8a6e5af04decca787a98c3822
TheLegendary-27 <vash.tosh001@gmail.com>
2021-10-14 17:10:49 -0400

    Corrected some floating point issues in calculating pole heights

--- script.js
+++ script.js
@@ -95,16 +95,20 @@ function printHeight() {
             } else {
                 return ((Math.ceil(height * 10) / 10) - 0.05).toFixed(2);
             }
-        } else if((height * 100) % 5 === 0) {
+        } else if((height * 1000) % 5 === 0) {
             if((height * 10) - Math.floor(height * 10) === 0){
+                console.log('I ran')
                 return (Math.floor(height * 10) / 10).toFixed(2);
             } else {
+                console.log('I ran');
                 return ((Math.ceil(height * 10) / 10) - 0.05).toFixed(2);
             }
         } else {
             if((height * 10) - Math.floor(height * 10) < 0.5) {
+                console.log('I ran');
                 return ((Math.ceil(height * 10) / 10) - 0.05).toFixed(2);
             } else {
+                console.log('I ran');
                 return (Math.ceil(height * 10) / 10).toFixed(2);
             }
         }
